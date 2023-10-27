41cdc597960083f4fc40c3a685ffb7d9f500faf0
matheusslongo <146895466+Matheusgta56@users.noreply.github.com>
2023-10-27 18:00:38 +0000

    settings1

--- app/components/Article.tsx
+++ app/components/Article.tsx
@@ -1,7 +1,7 @@
 export default function Article() {
     return(
         <main className="grid gap-4">
-  <div className="md:col-span-2 lg:col-span-3 mt-4 text-black text-center border-b border-[#F56A6A]">
+  <div className="md:col-span-2 lg:col-span-3 mt-4 text-black  border-b border-[#F56A6A]">
     <h2 id="cursos">
      Ipsum sed dolor <span>CURSOS</span>
     </h2>
@@ -9,9 +9,9 @@ export default function Article() {
   <div className=" rounded-md pb-2 py-8 border-b">
     <a href="/curso_html.html">
       <img src="/img/pic01.jpg.png" alt="" />
-      <div className="text-[#3D4449] text-center">
+      <div className="text-[#3D4449] text-center py-8">
         <h3>Interdum aenean</h3>
-        <p className="text-[#7F888F] text-center py-4">O curso é realizado por profissionais..</p>
+        <p className="text-[#7F888F] text-center py-4">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
         <a 
                         className="py-8 text-[#F56A6A]  bg-white border border-[#F56A6A] px-3 py-[3px]" href="#section" >
                         More
@@ -22,9 +22,9 @@ export default function Article() {
   <div className=" rounded-md pb-2 py-8 border-b">
     <a href="/curso_css.html">
       <img src="/img/a.png" alt="" />
-      <div className="text-[#3D4449] text-center">
+      <div className="text-[#3D4449] text-center py-8">
         <h3>Nulla amet dolore</h3>
-        <p className="text-[#7F888F] text-center py-4">O curso é realizado por profissionais..</p>
+        <p className="text-[#7F888F] text-center py-4">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
         <a 
                         className="py-8 text-[#F56A6A]  bg-white border border-[#F56A6A] px-3 py-[3px]" href="#section" >
                          More
@@ -35,9 +35,9 @@ export default function Article() {
   <div className=" rounded-md pb-2 py-8 border-b">
     <a href="/curso_js.html">
       <img src="/img/a (1).png" alt="" />
-      <div className="text-[#3D4449] text-center">
+      <div className="text-[#3D4449] text-center py-8">
         <h3>Tempus ullamcorper</h3>
-        <p className="text-[#7F888F] text-center py-4">O curso é realizado por profissionais..</p>
+        <p className="text-[#7F888F] text-center py-4">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
         <a 
                         className="py-8 text-[#F56A6A]  bg-white border border-[#F56A6A] px-3 py-[3px]" href="#section" >
                          More
@@ -48,9 +48,9 @@ export default function Article() {
   <div className="rounded-md pb-2 py-8 border-b">
     <a href="/curso_html.html">
       <img src="/img/a (2).png" alt="" />
-      <div className="text-[#3D4449] text-center">
+      <div className="text-[#3D4449] text-center py-8">
         <h3>Sed etiam facilis</h3>
-        <p className="text-[#7F888F] text-center py-4">O curso é realizado por profissionais..</p>
+        <p className="text-[#7F888F] text-center py-4">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
         <a 
                         className="py-8 text-[#F56A6A]  bg-white border border-[#F56A6A] px-3 py-[3px]" href="#section" >
                          More
@@ -61,9 +61,9 @@ export default function Article() {
   <div className=" rounded-md pb-2 py-8 border-b">
     <a href="/curso_html.html">
       <img src="/img/pic05.jpg.png" alt="" />
-      <div className="text-[#3D4449] text-center">
+      <div className="text-[#3D4449] text-center py-8">
         <h3>Feugiat lorem aenean</h3>
-        <p className="text-[#7F888F] text-center py-4">O curso é realizado por profissionais..</p>
+        <p className="text-[#7F888F] text-center py-4">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
         <a 
                         className="py-8 text-[#F56A6A]  bg-white border border-[#F56A6A] px-3 py-[3px]" href="#section" >
                          More
@@ -74,9 +74,9 @@ export default function Article() {
   <div className="rounded-md pb-2 py-8 border-b">
     <a href="/curso_html.html">
       <img src="/img/a (3).png" alt="" />
-      <div className="text-[#3D4449] text-center">
-        <h3>Amet varius aliquam</h3>
-        <p className="text-[#7F888F] text-center py-4">O curso é realizado por profissionais..</p>
+      <div className="text-[#3D4449] text-center py-8">
+        <h1>Amet varius aliquam</h1>
+        <p className="text-[#7F888F] text-center py-4">Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
         <a 
                         className="py-8 text-[#F56A6A]  bg-white border border-[#F56A6A] px-3 py-[3px]" href="#section" >
                          More
--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -6,11 +6,11 @@ export default  function Banner() {
                     <img src="/img/pic10.jpg.png" alt="Descrição da Imagem" />
                 </div>
                 <div className="py-8 basis-1/2 text-center text-black">
-                    <h2 className="py-8">Hi, I`m Teacher by UTFPR</h2>
+                    <h2 className="py-8 w-96 h-8 top-52 left-10 text-3xl font-bold leading-10 font-roboto-slab text-[#3D4449]">Hi, I m Teacher by UTFPR</h2>
                     
                     <h3 className="py-8 text-[#7F888F]">Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. Pellentesque sapien ac quam. Lorem ipsum dolor sit nullam.</h3>
                     <a 
-                        className="text-[#F56A6A]  bg-white border border-[#F56A6A] px-3 py-[3px] py-8" href="#section" >
+                        className="text-[#F56A6A]   bg-white border border-[#F56A6A] px-3 py-[3px] py-8" href="#section" >
                         Learn More
                     </a>
                 </div>
