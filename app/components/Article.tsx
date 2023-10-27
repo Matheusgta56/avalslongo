9ce300c09b976c58a12cf2a09f794fc4742c667f
matheusslongo <146895466+Matheusgta56@users.noreply.github.com>
2023-10-27 16:56:08 +0000

    settings0

--- app/components/Article.tsx
+++ app/components/Article.tsx
@@ -1,7 +1,7 @@
 export default function Article() {
     return(
         <main className="grid gap-4">
-  <div className="md:col-span-2 lg:col-span-3 mt-4 text-black text-center">
+  <div className="md:col-span-2 lg:col-span-3 mt-4 text-black text-center border-b border-[#F56A6A]">
     <h2 id="cursos">
      Ipsum sed dolor <span>CURSOS</span>
     </h2>
@@ -10,7 +10,7 @@ export default function Article() {
     <a href="/curso_html.html">
       <img src="/img/pic01.jpg.png" alt="" />
       <div className="text-[#3D4449] text-center">
-        <h3>Curso de HTML</h3>
+        <h3>Interdum aenean</h3>
         <p className="text-[#7F888F] text-center py-4">O curso é realizado por profissionais..</p>
         <a 
                         className="py-8 text-[#F56A6A]  bg-white border border-[#F56A6A] px-3 py-[3px]" href="#section" >
@@ -23,7 +23,7 @@ export default function Article() {
     <a href="/curso_css.html">
       <img src="/img/a.png" alt="" />
       <div className="text-[#3D4449] text-center">
-        <h3>Curso de CSS</h3>
+        <h3>Nulla amet dolore</h3>
         <p className="text-[#7F888F] text-center py-4">O curso é realizado por profissionais..</p>
         <a 
                         className="py-8 text-[#F56A6A]  bg-white border border-[#F56A6A] px-3 py-[3px]" href="#section" >
@@ -36,7 +36,7 @@ export default function Article() {
     <a href="/curso_js.html">
       <img src="/img/a (1).png" alt="" />
       <div className="text-[#3D4449] text-center">
-        <h3>Curso de JS</h3>
+        <h3>Tempus ullamcorper</h3>
         <p className="text-[#7F888F] text-center py-4">O curso é realizado por profissionais..</p>
         <a 
                         className="py-8 text-[#F56A6A]  bg-white border border-[#F56A6A] px-3 py-[3px]" href="#section" >
@@ -49,7 +49,7 @@ export default function Article() {
     <a href="/curso_html.html">
       <img src="/img/a (2).png" alt="" />
       <div className="text-[#3D4449] text-center">
-        <h3>Curso de DESIGN</h3>
+        <h3>Sed etiam facilis</h3>
         <p className="text-[#7F888F] text-center py-4">O curso é realizado por profissionais..</p>
         <a 
                         className="py-8 text-[#F56A6A]  bg-white border border-[#F56A6A] px-3 py-[3px]" href="#section" >
@@ -62,7 +62,7 @@ export default function Article() {
     <a href="/curso_html.html">
       <img src="/img/pic05.jpg.png" alt="" />
       <div className="text-[#3D4449] text-center">
-        <h3>Curso de GAMES</h3>
+        <h3>Feugiat lorem aenean</h3>
         <p className="text-[#7F888F] text-center py-4">O curso é realizado por profissionais..</p>
         <a 
                         className="py-8 text-[#F56A6A]  bg-white border border-[#F56A6A] px-3 py-[3px]" href="#section" >
@@ -75,7 +75,7 @@ export default function Article() {
     <a href="/curso_html.html">
       <img src="/img/a (3).png" alt="" />
       <div className="text-[#3D4449] text-center">
-        <h3>Curso de ROBÓTICA</h3>
+        <h3>Amet varius aliquam</h3>
         <p className="text-[#7F888F] text-center py-4">O curso é realizado por profissionais..</p>
         <a 
                         className="py-8 text-[#F56A6A]  bg-white border border-[#F56A6A] px-3 py-[3px]" href="#section" >
